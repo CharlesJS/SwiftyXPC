0acb038b82c5cfd1c1eba7b1b1bf49a375027919
Charles Srstka <support@charlessoft.com>
2021-08-21 01:34:11 -0400

    Add convenience method on CFArray to make getting objects out of it less painful

--- Sources/SwiftyXPC/Extensions/CFArray+SwiftyXPC.swift
+++ Sources/SwiftyXPC/Extensions/CFArray+SwiftyXPC.swift
@@ -8,6 +8,18 @@
 import CoreFoundation
 import XPC
 
+extension CFArray {
+    public subscript<I: BinaryInteger>(index: I) -> CFTypeRef? {
+        unsafeBitCast(CFArrayGetValueAtIndex(self, CFIndex(index)), to: CFTypeRef?.self)
+    }
+
+    public subscript<I: BinaryInteger, T: CFTypeRef>(index: I, as typeID: CFTypeID) -> T? {
+        guard let value = self[index], CFGetTypeID(value) == typeID else { return nil }
+
+        return value as? T
+    }
+}
+
 extension CFArray: XPCConvertible {
     public static func fromXPCObject(_ xpcObject: xpc_object_t) -> Self? {
         var callBacks = kCFTypeArrayCallBacks
